5a04741d073d605e4c51681853d8387215c1aa0a
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2020-12-09 09:09:33 +0100

    removed 'th' className

--- src/Components/TableHead.js
+++ src/Components/TableHead.js
@@ -4,20 +4,20 @@ function TableHead() {
   return (
     <thead>
       <tr className="column-heading-row">
-        <th className="column-headings">ID</th>
-        <th className="column-headings">Name</th>
-        <th className="column-headings">Username</th>
-        <th className="column-headings">Company</th>
-        <th className="column-headings">Focus</th>
-        <th className="column-headings">Catchphrase</th>
-        <th className="column-headings">Website</th>
-        <th className="column-headings">Email</th>
-        <th className="column-headings">Phone</th>
-        <th className="column-headings">City</th>
-        <th className="column-headings">Address</th>
-        <th className="column-headings">Zipcode</th>
-        <th className="column-headings">Latitude</th>
-        <th className="column-headings">Longitude</th>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Company</th>
+        <th>Focus</th>
+        <th>Catchphrase</th>
+        <th>Website</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>City</th>
+        <th>Address</th>
+        <th>Zipcode</th>
+        <th>Latitude</th>
+        <th>Longitude</th>
       </tr>
     </thead>
   );
